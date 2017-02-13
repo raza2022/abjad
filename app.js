de136d8b2c7cae7d9693dd81d683f4634ebb0384
raza2022 <raza2022@gmail.com>
2017-02-13 22:41:23 +0500

    abjad values updated

--- app.js
+++ app.js
@@ -1,64 +1,37 @@
 /***** initialize urdu words ******/
-//TODO: capital values need to be implemented with (word + shift || caps lock)
-var urduWords = {
-    ا : 65,
-    ب : 66,
-    چ : 67,
-    د : 68,
-    ع : 69,
-    ف : 70,
-    گ : 71,
-    ھ : 72,
-    ی : 73,
-    ج : 74,
-    ک : 75,
-    ل : 76,
-    م : 77,
-    ن : 78,
-    ہ : 79,
-    پ : 80,
-    ق : 81,
-    ر : 82,
-    س : 83,
-    ت : 84,
-    ئ : 85,
-    ط : 86,
-    و : 87,
-    ش : 88,
-    ے : 89,
-    ز : 90 };
 
-/***** give values to key codes ******/
-//remember here the original abjad words values given
 var abjadValues = {
-    65 : 1,
-    66 : 2,
-    67 : 3,
-    68 : 4,
-    69 : 70,
-    70 : 80,
-    71 : 5,
-    72 : 10,
-    73 : 10,
-    74 : 20,
-    75 : 30,
-    76 : 40,
-    77 : 50,
-    78 : 5,
-    79 : 5,
-    80 : 100,
-    81 : 100,
-    82 : 200,
-    83 : 60,
-    84 : 400,
-    85 : 10,
-    86 : 9,
-    87 : 6,
-    88 : 300,
-    89 : 89,
-    90 : 7
+    ا : 1,
+    ب : 2,
+    ت : 400,
+    ث : 500,
+    ج : 3,
+    ح : 8,
+    خ : 600,
+    د : 4,
+    ذ : 700,
+    ر : 200,
+    ز : 7,
+    س : 60,
+    ش : 300,
+    ص : 90,
+    ض : 800,
+    ط : 9,
+    ظ : 900,
+    ع : 70,
+    غ : 1000,
+    ف : 80,
+    ق : 100,
+    ک : 20,
+    ل : 30,
+    م : 40,
+    ن : 50,
+    و : 6,
+    ھ : 5,
+    ہ : 5,
+    ء : 1,
+    ی : 10
 };
-
 
 document.addEventListener("keyup", function () {
         var words = document.getElementById("key").value;
@@ -66,24 +39,21 @@ document.addEventListener("keyup", function () {
         //
         var text = document.getElementById("key").value;
         var len = text.length;
-        console.log(len);
-        if(len){
+        if( len ){
             //get counter by id
             var counter = document.getElementById('counter');
 
             //that filter just for legacy and remove english words
             //TODO: that filter will be removed when all urdu words added
             splitWords = splitWords.filter(function(word){
-                return urduWords[word]
+                return abjadValues[word]
             });
 
             //wih chaining method got total count of words
             var values = splitWords = splitWords.map(function(obj){
-                //convert to key codes
-                return urduWords[obj];
                 //got original values of words
-            }).map(function(val){
-                return abjadValues[val]
+                return abjadValues[obj];
+
             });
             counter.innerText = sum(values)
         }
